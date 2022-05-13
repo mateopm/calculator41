ab6e778b6353dc6af462521be7e5e1d3ecf526b8
mateopm <mateopm72@educastur.es>
2022-05-13 17:31:12 +0200

    Añadido esqueleto Spring Boot

---
+++ src/main/java/com/ciber41/calculator41/Calculator41Application.java
@@ -0,0 +1,13 @@
+package com.ciber41.calculator41;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calculator41Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calculator41Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ciber41/calculator41/Calculator41ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ciber41.calculator41;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Calculator41ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
