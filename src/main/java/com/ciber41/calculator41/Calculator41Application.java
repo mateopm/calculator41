66fcc529c9d915314616f4c8e204f3b7b44d5880
mateopm <mateo38@hotmail.es>
2022-05-20 19:51:54 +0200

    funciona porfa

--- src/main/java/com/ciber41/calculator41/Calculator41Application.java
+++ src/main/java/com/ciber41/calculator41/Calculator41Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Calculator41Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Calculator41Application.class, args);
-	}
+public static void main(String[] args) {
+SpringApplication.run(Calculator41Application.class, args);
+}
 
 }
